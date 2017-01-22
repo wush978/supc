c847c114a4130fd7e5ba2df8153038d930f85b05
Wush Wu <wush978@gmail.com>
2017-01-23 03:02:37 +0800

    correct the unit test will not match the function

--- tests/test-supc1.R
+++ tests/test-supc1.R
@@ -17,37 +17,43 @@ X.supc.ref <- structure(list(cluster = c(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L,
 8L, 9L), centers = structure(c(-0.0180254152322607, 2.046727139439, 
 1.08716939783766, 6.20911417627993, 8.0147761104807, 6.79821207689788, 
 3.1200608974441, 5.08556992438004, 3.97988236811187, -0.162390279741713, 
-0.0922984238508252, 1.10583358958467, 0.0381059301321746, -0.0860889759850033, 
-1.12872658834518, 2.95353852156743, 2.9359322400503, 3.9557877283095
+0.0922984238508252, 1.10583358958467, 0.0381059301321746, -0.0860889759850034, 
+1.12872658834518, 2.95353852156743, 2.93593224005029, 3.95578772830951
 ), .Dim = c(9L, 2L), .Dimnames = list(c("1", "2", "3", "4", "5", 
 "6", "7", "8", "9"), NULL)), size = structure(c(3L, 3L, 3L, 3L, 
 3L, 3L, 3L, 3L, 3L), .Dim = 9L, .Dimnames = structure(list(cl = c("1", 
 "2", "3", "4", "5", "6", "7", "8", "9")), .Names = "cl"), class = "table")), .Names = c("cluster", 
-"centers", "size"), class = "supc", iteration = 5)
+"centers", "size"))
 
-stopifnot(isTRUE(all.equal(X.supc, X.supc.ref)))
+ref.check.name <- c("cluster", "centers", "size")
+stopifnot(isTRUE(all.equal(X.supc[ref.check.name], X.supc.ref)))
 X.supc <- supc1(X, r = 0.9, t = 0.75, implementation = "cpp")
 print(X.supc)
-stopifnot(isTRUE(all.equal(X.supc, X.supc.ref)))
+stopifnot(isTRUE(all.equal(X.supc[ref.check.name], X.supc.ref)))
 
 X.supc <- supc1(X, r = c(0.9, 0.9), t = c(0.75, 0.75))
-stopifnot(isTRUE(all.equal(X.supc, rep(list(X.supc.ref), 2))))
+stopifnot(isTRUE(all.equal(lapply(X.supc, function(.) .[ref.check.name]), rep(list(X.supc.ref), 2))))
 stopifnot(isTRUE(all.equal(
   supc1(X, r = quantile(dist(X), 0.85), t = 0.75),
   supc1(X, rp = 0.85, t = 0.75)
 )))
 
+list.check.names <- c("x", "d0", "r", "cluster", "centers", "size")
 stopifnot(isTRUE(all.equal(
-  supc1(X, r =quantile(dist(X), seq(0.1, 0.5, by = 0.1)), t = quantile(dist(X), seq(0.1, 0.5, by = 0.1)) / 5),
-  supc1(X, rp = seq(0.1, 0.5, by = 0.1), t = "static")
+  lapply(supc1(X, r =quantile(dist(X), seq(0.1, 0.5, by = 0.1)), t = quantile(dist(X), seq(0.1, 0.5, by = 0.1)) / 5), function(obj) {
+    obj[list.check.names]
+  }),
+  lapply(supc1(X, rp = seq(0.1, 0.5, by = 0.1), t = "static"), function(obj) {
+    obj[list.check.names]
+  })
 )))
 
 stopifnot(isTRUE(all.equal(
-  supc1(X, r =quantile(dist(X), seq(0.1, 0.5, by = 0.1)), t = lapply(quantile(dist(X), seq(0.1, 0.5, by = 0.1)), function(.r) {
+  lapply(supc1(X, r =quantile(dist(X), seq(0.1, 0.5, by = 0.1)), t = lapply(quantile(dist(X), seq(0.1, 0.5, by = 0.1)), function(.r) {
     force(.r)
     function(t) .r / 20 + (.r / 50) * t
-  })),
-  supc1(X, rp = seq(0.1, 0.5, by = 0.1), t = "dynamic")
+  })), function(obj) obj[list.check.names]),
+  lapply(supc1(X, rp = seq(0.1, 0.5, by = 0.1), t = "dynamic"), "[", list.check.names)
 )))
 
 stopifnot(!isTRUE(all.equal(
@@ -57,10 +63,10 @@ stopifnot(!isTRUE(all.equal(
 
 dist.mode("amap")
 X.supc <- supc1(X, r = 0.9, t = 0.75)
-stopifnot(isTRUE(all.equal(X.supc, X.supc.ref)))
+stopifnot(isTRUE(all.equal(X.supc[ref.check.name], X.supc.ref)))
 
 if (require(gputools)) {
   dist.mode("gputools")
   X.supc <- supc1(X, r = 0.9, t = 0.75)
-  stopifnot(isTRUE(all.equal(X.supc, X.supc.ref)))
+  stopifnot(isTRUE(all.equal(X.supc[ref.check.name], X.supc.ref)))
 }
