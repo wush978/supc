c35a6841e71a2834790c882c349a327b2d12f831
Wush Wu <wush978@gmail.com>
2017-02-19 14:12:17 +0800

    specifically set `implementation = "R"`

--- tests/test-golub.R
+++ tests/test-golub.R
@@ -2,6 +2,6 @@ library(supc)
 data("golub", package = "supc")
 if (Sys.getenv("TEST_GOLUB") == "TRUE") {
   system.time(golub.cpp <- supc1(golub, r = 4, t = "dynamic", implementation = "cpp", verbose = TRUE))
-  system.time(golub.r <- supc1(golub, r = 4, t = "dynamic", verbose = TRUE))
+  system.time(golub.r <- supc1(golub, r = 4, t = "dynamic", implementation = "R", verbose = TRUE))
   stopifnot(isTRUE(all.equal(golub.cpp, golub.r)))
 }
