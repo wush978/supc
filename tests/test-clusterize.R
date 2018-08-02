9a626cbb5922b71bdb0483b4656e2ecdeafde840
Wush Wu <wush978@gmail.com>
2018-08-02 10:53:32 +0800

    - put m2 into sysdata

--- tests/test-clusterize.R
+++ tests/test-clusterize.R
@@ -1,12 +1,10 @@
 library(supc)
 library(dbscan)
 
+tol <- 1e-4
 for(i in 1:10) {
-  m <- matrix(0, 2e2, 5)
-  m[] <- rnorm(length(m))
-  m2 <- supc.random(m, k = 10, rp = 0.5, t = "dynamic", tolerance = tol <- 1e-4)$result
-  g1 <- dbscan::dbscan(m2, eps = tol, minPts = 1)$cluster
-  g2 <- supc:::.clusterize(m2, tol)
+  g1 <- dbscan::dbscan(supc:::.test.m, eps = tol, minPts = 1)$cluster
+  g2 <- supc:::.clusterize(supc:::.test.m, tol)
   stopifnot(g1 == g2)
 }
 
